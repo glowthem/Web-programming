d7fedb67309fd6baccf34293499ec4bf4aa7dd5c
glowthem <glowing713@naver.com>
2020-03-24 00:52:55 +0900

    2020-03-24 JS Study#1

--- Study/JavaScript/main.js
+++ Study/JavaScript/main.js
@@ -1,19 +1,8 @@
-function Person(firstName, lastName, dob) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.dob = new Date(dob);
-}
+// Single element
+console.log(document.getElementById("my-form"));
+console.log(document.querySelector("label"));
 
-Person.prototype.getBirthYear = function() {
-  return this.dob.getFullYear();
-};
-
-Person.prototype.getFullName = function() {
-  return `${this.firstName} ${this.lastName}`;
-};
-
-const Person1 = new Person("John", "Doe", "4-3-1980");
-const Person2 = new Person("Mary", "Smith", "3-6-1970");
-
-console.log(Person1);
-console.log(Person2);
+// Multiple element
+console.log(document.querySelectorAll(".item"));
+console.log(document.getElementsByClassName("item"));
+console.log(document.getElementsByTagName("h1"));
