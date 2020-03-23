0b088d31bf60d5541954ac73a830deba948e336b
glowthem <glowing713@naver.com>
2020-03-23 23:50:50 +0900

    2020-03-23 JS Study#1

--- Study/JavaScript/main.js
+++ Study/JavaScript/main.js
@@ -1,67 +1,19 @@
-// Array which contains objects
-const todos = [
-  {
-    id: 1,
-    text: "Take out trash",
-    isCompleted: true
-  },
-  {
-    id: 2,
-    text: "Meeting with boss",
-    isCompleted: true
-  },
-  {
-    id: 3,
-    text: "Dentist appt",
-    isCompleted: false
-  }
-];
-
-console.log(todos);
-
-console.log(todos[1].text);
-
-// Convert object into JSON
-const todoJSON = JSON.stringify(todos);
-console.log(todoJSON);
-
-// For
-for (let i = 0; i <= 10; i++) {
-  console.log(`For loop Number: ${i}`);
+function Person(firstName, lastName, dob) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.dob = new Date(dob);
 }
 
-for (let i = 0; i < todos.length; i++) {
-  console.log(todos[i].text);
-}
+Person.prototype.getBirthYear = function() {
+  return this.dob.getFullYear();
+};
 
-// For-of
-for (const item of todos) {
-  console.log(item.id);
-}
+Person.prototype.getFullName = function() {
+  return `${this.firstName} ${this.lastName}`;
+};
 
-// For-each
-todos.forEach(function(todo) {
-  console.log(todo.text);
-});
+const Person1 = new Person("John", "Doe", "4-3-1980");
+const Person2 = new Person("Mary", "Smith", "3-6-1970");
 
-// map
-const todoText = todos.map(function(todo) {
-  return todo.text;
-});
-console.log(todoText);
-
-// filter
-const todoCompleted = todos.filter(function(todo) {
-  return todo.isCompleted === true;
-});
-console.log(todoCompleted);
-
-// map and just get text from filtered one
-const todoCompleted_text = todos
-  .filter(function(todo) {
-    return todo.isCompleted === true;
-  })
-  .map(function(todo) {
-    return todo.text;
-  });
-console.log(todoCompleted_text);
+console.log(Person1);
+console.log(Person2);
